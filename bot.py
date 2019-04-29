b24fb41954089ee005c60b9fd7a49f7c00117b16
Dmitry S. Vlasov <z1nkum@gmail.com>
2019-04-29 17:35:20 +0300

    fix polling timeout (5sec by default - is not enough)

--- bot.py
+++ bot.py
@@ -320,7 +320,7 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
+    updater.start_polling(timeout=60)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
